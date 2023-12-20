a7b61e5f8bd424a5e46111de2550d5fc077e9f5e
Riyansh Biswas <charmingbiswas@gmail.com>
2023-12-20 18:00:44 +0530

    Added Sieve Of Eratosthenes algorithm for counting prime numbers

---
+++ Algorithms/SieveOfEratosthenes/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+
+int countPrimes(int n) {
+    int count = 0;
+    std::vector<bool> vec(n + 1, true);
+    for(int i = 2; i < n; i++) {
+        if(vec[i]) {
+            count++;
+            for(int j = 2*i; j < n; j = j + i) {
+                vec[j] = false;
+            }
+        }
+    }
+    return count;     
+}
+
+int main()
+{
+    std::cout << countPrimes(10) << std::endl;
+    return 0;
+};
